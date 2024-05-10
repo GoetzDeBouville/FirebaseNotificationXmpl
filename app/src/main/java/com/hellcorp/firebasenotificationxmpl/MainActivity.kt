befd5f2848e44c5bb41de399005b9d1be3087fd0
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-05-10 18:36:37 +0300

    Add request permisson

--- app/src/main/java/com/hellcorp/firebasenotificationxmpl/MainActivity.kt
+++ app/src/main/java/com/hellcorp/firebasenotificationxmpl/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.hellcorp.firebasenotificationxmpl
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.messaging.FirebaseMessaging
 
@@ -12,13 +18,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        requestNopotificationPermission()
+
+        val processors = Runtime.getRuntime().availableProcessors()
+        Log.e("MyLog", "availableProcessors = $processors")
 
         FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
             if (!task.isSuccessful) {
                 return@OnCompleteListener
             }
-
+            Log.e("MyLog", "task = $task \ntask.result = ${task.result}")
             findViewById<TextView>(R.id.tv_text).text = task.result
         })
+    }
+
+    private fun requestNopotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            val hasPermission = ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
+
+            if (hasPermission.not()) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                    0
+                )
+            }
+        }
     }
 }
