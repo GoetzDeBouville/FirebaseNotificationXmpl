ed61a070172ae844eec394e84efc2cfd36c9b5d7
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-05-10 13:44:30 +0300

    Provide token to screen

--- app/src/main/java/com/hellcorp/firebasenotificationxmpl/MainActivity.kt
+++ app/src/main/java/com/hellcorp/firebasenotificationxmpl/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.hellcorp.firebasenotificationxmpl
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                return@OnCompleteListener
+            }
+
+            findViewById<TextView>(R.id.tv_text).text = task.result
+        })
     }
 }
